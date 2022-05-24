6808aabf44053e8e5cfa259aa9a7ce31885f6212
KINHYEONJI <hyeonji0718@gmail.com>
2022-05-24 20:54:23 +0900

    feat(baseballgame) : 경고 메세지 출력 메소드 구현

---
+++ src/baseballgame.js
@@ -0,0 +1,23 @@
+class BaseballGame {
+  play(computerInputNumber, playerInputNumber) {
+    // playerInputNumber 의 유효성 판단
+    // computerNumber와 playInputNumber를 비교
+    return "";
+  }
+
+  decideResult() {
+    //play의 결과에 따라 return 값이 달라진다.
+    //사용자가 game에서 승리하지 않았을 시 false 리턴해 hint를 출력하고 play를 반복 수행하게 한다.
+    //사용자가 game에서 승리할 경우 true를 리턴헤 replay를 할 수 있게 한다.
+  }
+
+  printHint() {
+    //힌트를 출력한다.
+  }
+
+  isPlayerInputNumberValid(playerInputNumber) {}
+
+  alertErrorMessage() {
+    window.alert("유효하지 않은 입력입니다.");
+  }
+}
