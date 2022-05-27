91f10c4428b985cf83ba1445814480f8e7f72e6d
KINHYEONJI <hyeonji0718@gmail.com>
2022-05-27 13:34:16 +0900

    refactor(computer) : 임시 변수를 질의 함수로 변경 - randomNumber를 구하는 연산을 질의 함수로 추출

--- src/computer.js
+++ src/computer.js
@@ -2,13 +2,18 @@ export default class Computer {
   constructor() {
     this.number = this.generateRandomNumber();
   }
+
   generateRandomNumber() {
     const generateNumbers = [];
     while (generateNumbers.length !== 3) {
-      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      const randomNumber = this.pickNumberInRange(1, 9);
       if (!generateNumbers.includes(randomNumber))
         generateNumbers.push(randomNumber);
     }
     return generateNumbers.join("");
+  }
+
+  pickNumberInRange(start, end) {
+    return MissionUtils.Random.pickNumberInRange(start, end);
   }
 }
