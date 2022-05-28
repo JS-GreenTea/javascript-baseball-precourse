c10143b346b3cfcce244519a093d750dd854ea71
spiderq2697 <spiderq10@gmail.com>
2022-05-28 23:28:16 +0900

    feat: ui에 '볼'과 '스트라이크' 출력을 수정한다

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -40,10 +40,10 @@ class BaseballGame {
 
   ballStateToString() {
     let result = '';
-    if (this.#ballState.ball) result += `${this.#ballState.ball}볼`;
+    if (this.#ballState.ball) result += `${this.#ballState.ball}볼 `;
     if (this.#ballState.strike) result += `${this.#ballState.strike}스트라이크`;
     if (this.#ballState.nothing === 3) result = '낫싱';
-    return result;
+    return result.trim();
   }
 
   announceBallState() {
