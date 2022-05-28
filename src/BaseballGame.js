56018e36a91d979b5a14d8193e4327f5931c3f55
spiderq2697 <spiderq10@gmail.com>
2022-05-28 22:18:05 +0900

    feat: ball인지 확인한다

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -3,8 +3,13 @@ class BaseballGame {
   constructor() {
     this.#ballState = new BallState();
   }
+
   isStrike(computerInputNumber, userInputNumber) {
     return computerInputNumber === userInputNumber;
+  }
+
+  isBall(computerInputNumbers, userInputNumber) {
+    return computerInputNumbers.find(number => number === userInputNumber);
   }
 }
 
