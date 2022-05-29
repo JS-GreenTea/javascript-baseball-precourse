2f662939b354ef3b44aca6a2d52de2c761a47d69
KINHYEONJI <hyeonji0718@gmail.com>
2022-05-29 16:59:11 +0900

    refactor(constant) : 일관된 quote 사용

--- src/constant.js
+++ src/constant.js
@@ -6,13 +6,13 @@ export const RANGE = {
 export const LENGTH = 3;
 
 export const SYS = {
-  INVALID_INPUT: "유효하지 않은 입력입니다.",
-  WIN_MESSAGE: "정답을 맞추셨습니다 ~ ㅊㅊ",
+  INVALID_INPUT: '유효하지 않은 입력입니다.',
+  WIN_MESSAGE: '정답을 맞추셨습니다 ~ ㅊㅊ',
 };
 
 export const CALL = {
-  WIN: "승리",
-  BALL: "볼",
-  STRIKE: "스트라이크",
-  NOTHING: "낫싱",
+  WIN: '승리',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
 };
