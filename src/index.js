a6f5d290f9d6ac34185cc14508dfb6d6c56bfe97
spiderq2697 <spiderq10@gmail.com>
2022-05-30 21:13:13 +0900

    fix: 입력값을 파라미터로 넘겨준다

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const restartGame = () => {
 };
 
 const submitButtonClickHandler = event => {
-  if (!validateNumber()) {
+  if (!validateNumber(userInput.value)) {
     userInput.value = '';
     userInput.focus();
   }
