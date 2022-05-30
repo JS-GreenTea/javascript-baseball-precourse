5f96f52317ca043994a261e80856c775e990fa15
spiderq2697 <spiderq10@gmail.com>
2022-05-30 21:14:08 +0900

    fix: button클릭시 submit을 차단한다

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const restartGame = () => {
 };
 
 const submitButtonClickHandler = event => {
+  event.preventDefault();
   if (!validateNumber(userInput.value)) {
     userInput.value = '';
     userInput.focus();
